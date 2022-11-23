756d2c8d5260119e066f322048b9bf3a27e35639
L1v1ngston <115081879+L1v1ngston@users.noreply.github.com>
2022-11-23 20:47:55 +0300

    Обновление нулевой версии

---
+++ Client/jeducation-v0.1/mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "choosemode.h"
+#include <QMessageBox>
+#include <iostream>
+#include "info.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    //hide();
+    window = new ChooseMode(this);
+    window -> show();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    window1 = new info(this);
+    window1 -> show();
+}
+
