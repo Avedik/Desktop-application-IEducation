1edec92fc94341182a5478f45e8a1ec10a80aad8
Kuzemko03 <92890316+Kuzemko03@users.noreply.github.com>
2022-11-25 18:04:39 +0300

    Описал server.cpp

---
+++ Server/server.cpp
@@ -0,0 +1,139 @@
+#include "server.h"
+#include "serverworker.h"
+#include <QThread>
+#include <functional>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QTimer>
+
+Server::Server(QObject *parent): QTcpServer(parent){}
+
+void Server::jsonFromLoggedOut(ServerWorker *sender, const QJsonObject &docObj)
+{
+    Q_ASSERT(sender);
+    const QJsonValue typeVal = docObj.value(QLatin1String("type"));
+    if (typeVal.isNull() || !typeVal.isString())
+        return;
+    if (typeVal.toString().compare(QLatin1String("login"), Qt::CaseInsensitive) != 0)
+        return;
+    const QJsonValue usernameVal = docObj.value(QLatin1String("username"));
+    if (usernameVal.isNull() || !usernameVal.isString())
+        return;
+    const QString newUserName = usernameVal.toString().simplified();
+    if (newUserName.isEmpty())
+        return;
+    for (ServerWorker *worker : qAsConst(m_clients)) {
+        if (worker == sender)
+            continue;
+        if (worker->userName().compare(newUserName, Qt::CaseInsensitive) == 0) {
+            QJsonObject message;
+            message[QStringLiteral("type")] = QStringLiteral("login");
+            message[QStringLiteral("success")] = false;
+            message[QStringLiteral("reason")] = QStringLiteral("username already exists");
+            sendJson(sender, message);
+            return;
+        }
+    }
+    sender->setUserName(newUserName);
+    QJsonObject successMessage;
+    successMessage[QStringLiteral("type")] = QStringLiteral("login");
+    successMessage[QStringLiteral("success")] = true;
+    sendJson(sender, successMessage);
+    QJsonObject connectedMessage;
+    connectedMessage[QStringLiteral("type")] = QStringLiteral("new user");
+    connectedMessage[QStringLiteral("username")] = newUserName;
+    broadcast(connectedMessage, sender);
+}
+
+void Server::jsonFromLoggedIn(ServerWorker *sender, const QJsonObject &docObj)
+{
+    Q_ASSERT(sender);
+    const QJsonValue typeVal = docObj.value(QLatin1String("type"));
+    if (typeVal.isNull() || !typeVal.isString())
+        return;
+    if (typeVal.toString().compare(QLatin1String("message"), Qt::CaseInsensitive) != 0)
+        return;
+    const QJsonValue textVal = docObj.value(QLatin1String("text"));
+    if (textVal.isNull() || !textVal.isString())
+        return;
+    const QString text = textVal.toString().trimmed();
+    if (text.isEmpty())
+        return;
+    QJsonObject message;
+    message[QStringLiteral("type")] = QStringLiteral("message");
+    message[QStringLiteral("text")] = text;
+    message[QStringLiteral("sender")] = sender->userName();
+    broadcast(message, sender);
+}
+
+void Server::sendJson(ServerWorker *destination, const QJsonObject &message)
+{
+    Q_ASSERT(destination);
+    destination->sendJson(message);
+}
+
+void Server::broadcast(const QJsonObject &message, ServerWorker *exclude)
+{
+    for (ServerWorker *worker : m_clients) {
+        Q_ASSERT(worker);
+        if (worker == exclude)
+            continue;
+        sendJson(worker, message);
+    }
+}
+
+void Server::jsonReceived(ServerWorker *sender, const QJsonObject &doc)
+{
+    Q_ASSERT(sender);
+    emit logMessage(QLatin1String("JSON received ") + QString::fromUtf8(QJsonDocument(doc).toJson()));
+    if (sender->userName().isEmpty())
+        return jsonFromLoggedOut(sender, doc);
+    jsonFromLoggedIn(sender, doc);
+}
+
+void Server::userDisconnected(ServerWorker *sender)
+{
+    m_clients.removeAll(sender);
+    const QString userName = sender->userName();
+    if (!userName.isEmpty()) {
+        QJsonObject disconnectedMessage;
+        disconnectedMessage[QStringLiteral("type")] = QStringLiteral("user disconnected");
+        disconnectedMessage[QStringLiteral("username")] = userName;
+        broadcast(disconnectedMessage, nullptr);
+        emit logMessage(userName + QLatin1String(" disconnected"));
+    }
+    sender->deleteLater();
+}
+
+void Server::userError(ServerWorker *sender)
+{
+    Q_UNUSED(sender)
+    emit logMessage(QLatin1String("Error from ") + sender->userName());
+}
+
+void Server::incomingConnection(qintptr socketDescriptor)
+{
+    ServerWorker *worker = new ServerWorker(this);
+    if (!worker->setSocketDescriptor(socketDescriptor)) {
+        worker->deleteLater();
+        return;
+    }
+    connect(worker, &ServerWorker::disconnectedFromClient, this, std::bind(&Server::userDisconnected, this, worker));
+    connect(worker, &ServerWorker::error, this, std::bind(&Server::userError, this, worker));
+    connect(worker, &ServerWorker::jsonReceived, this, std::bind(&Server::jsonReceived, this, worker, std::placeholders::_1));
+    connect(worker, &ServerWorker::logMessage, this, &Server::logMessage);
+    m_clients.append(worker);
+    emit logMessage(QStringLiteral("New client connected"));
+}
+
+void Server::stopServer()
+{
+    for (ServerWorker *worker : m_clients) {
+        worker->disconnectFromClient();
+    }
+    close();
+}
+
+
+
