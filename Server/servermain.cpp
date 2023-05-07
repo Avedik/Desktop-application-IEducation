6503188fc821d4143a27b3f1581e349a4a584dbe
Dan11l54 <94974300+Dan11l54@users.noreply.github.com>
2023-05-07 12:37:46 +0300

    добавил базу данных

--- Server/servermain.cpp
+++ Server/servermain.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 #include "serverWindow.h"
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QMessageBox>
+#include "database.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    database database;
     ServerWindow serverWin;
     serverWin.show();
     return a.exec();
